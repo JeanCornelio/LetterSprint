8faac1d48b2604d028ad2cd57a5e515f8f00b82c
Jean Cornelio <jean_cornelio17@hotmail.com>
2024-10-21 01:51:43 -0400

    caret with rows words and words Position

--- src/components/WritingTest.tsx
+++ src/components/WritingTest.tsx
@@ -5,7 +5,7 @@ import { LoadingIcon, ReloadIcon } from "../icons/Icons";
 import { TIMER } from "../constants";
 
 const text =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
 interface Letters {
   letter: string;
@@ -24,13 +24,16 @@ export const WritingTest = () => {
 
   const [wordPosition, setWordPosition] = useState(0);
   const [wordGoal, setWordGoal] = useState<string | null>(null);
-  const [caretPosition, setCaretPosition] = useState({ y: 5.5, x: 8 });
+  const [caretPosition, setCaretPosition] = useState({ y: 10, x: 8.5 });
   const letterPosition = useRef(0);
   const [test, setTest] = useState<Word[]>([]);
 
   const { loading, handleLoading } = useLoading();
   const { seconds, handleTimerState, timerState, handleTimerTime } = useTimer();
-
+  const testContent = useRef(null)
+  const caret = useRef(null)
+  const [rowPosition, setRowPosition] = useState(0);
+  const [row, setRow] = useState(0);
   const createTest = () => {
     handleLoading(true);
 
@@ -126,29 +129,82 @@ export const WritingTest = () => {
 
 
 
+
+
+
+
       //Escapar el espacion que salte la linea, solamente si se ha escrito algo si LetterPosition.current > 0  
       if (code === 'Space' && letterPosition.current > 0) {
         //Reset Letter position 
         letterPosition.current = 0
         //Go to next word
         setWordPosition((current) => current + 1);
+        //Se suman las cantidades de posiciones en la word, esto se reinicia luego.
+        setRowPosition((current) => current + 1)
+        const lettersNoWrited = test[wordPosition].letters.filter((el) => el.state === '')
+
+        const total = (lettersNoWrited.length + 1) * 18 // * 18 to N letters and 1+ with a Space
+
         currentWord.state = tocheckWordCorreclyCompleted(currentWord);
-        TODO://RECUERDA validar el salto de letra para el caret
-        //Ir a lasiguiente palabra con el caret
+
         setCaretPosition((prev) => {
           return {
             ...prev,
-            x: prev.x += 17
+            x: prev.x += total
           };
         })
-        return
+
       }
+
+
+      let contentChildren = Array.from(testContent.current.children).map((child, index) => {
+
+        return child.getBoundingClientRect().top
+
+
+      });
+
+
+      const rowsGroups = Object.groupBy(contentChildren, (el) => el)
+
+      const totalOfRows = Object.keys(rowsGroups).map((el) => {
+
+        return rowsGroups[el]
+      })
+
+      //Remover la posicion 0
+      totalOfRows.shift()
+
+      console.log(totalOfRows[0].length, rowPosition, totalOfRows[0].lenght > wordPosition + 1)
+      //console.log(testContent.current.getBoundingClientRect().right)
+
+      if (totalOfRows[row].length === rowPosition + 1 && code === 'Space') {
+        console.log('irse a la otra linea')
+
+        setCaretPosition((prev) => {
+          return {
+            ...prev,
+            x: prev.x = 8.5,
+            y: prev.y += 52
+          };
+        })
+
+        setRow(prev => prev += 1)
+        // La posicion de la palabra en la fila de palabras, Validar cuando el caret llegue a la ultima palabra y se pueda borrar
+        setRowPosition(0)
+      }
+
+
+
+
 
 
       // Cuando se preciona el spacio y no se ha intentando jugar ninguna letra
       if (code === 'Space') {
         return
       }
+
+
 
 
 
@@ -213,6 +269,7 @@ export const WritingTest = () => {
       setTest(updateTest);
 
 
+
       //Pasar a la siguiente letra
       if (key.length === 1) {
         letterPosition.current += 1;
@@ -221,46 +278,57 @@ export const WritingTest = () => {
         setCaretPosition((prev) => {
           return {
             ...prev,
-            x: prev.x += 17
+            x: prev.x += 18
           };
         })
       }
 
 
 
+      if (key === "Backspace") {
+
+        // Borrar unicamente cuando la la posicion de la lettra sea 0 significa que llegaste al final de la letra y no puedes borrar mas
+        if (letterPosition.current > 0) {
+          letterPosition.current -= 1
+
+          //mover el caret cuando se borre una letra
+          setCaretPosition((prev) => {
+            return {
+              ...prev,
+              x: prev.x -= 18
+            };
+          })
+        }
 
 
-      // Borrar unicamente cuando la la posicion de la lettra sea 0 significa que llegaste al final de la letra y no puedes borrar mas
-      if (key === "Backspace" && letterPosition.current > 0) {
-        letterPosition.current -= 1
+        //si la palabra escrita anteriormente tuvo un error enviarlo a la palabra anterior
 
-        //mover el caret cuando se borre una letra
-        setCaretPosition((prev) => {
-          return {
-            ...prev,
-            x: prev.x -= 17
-          };
-        })
+        if (wordPosition > 0 && letterPosition.current === 0 && test[wordPosition - 1].state === 'error typed') {
+
+          setWordPosition((current) => current - 1);
+          //enviarle la cantidad de palabras de la anterior
+          letterPosition.current = test[wordPosition - 1].letters.length;
+          //Remover el error para saber si la escribira bien
+          test[wordPosition - 1].state = ''
+          //Ir a la palabra anterior con el caret si esta esta mal escrita y tuvo un error
+          setCaretPosition((prev) => {
+            return {
+              ...prev,
+              x: prev.x -= 18
+            };
+          })
+          return
+
+        }
+
+
       }
 
-      //si la palabra escrita anteriormente tuvo un error enviarlo a la palabra anterior
 
-      if (wordPosition > 0 && key === "Backspace" && letterPosition.current === 0 && test[wordPosition - 1].state === 'error typed') {
 
-        setWordPosition((current) => current - 1);
-        //enviarle la cantidad de palabras de la anterior
-        letterPosition.current = test[wordPosition - 1].letters.length
 
-        //Ir a la palabra anterior con el caret si esta esta mal escrita y tuvo un error
-        setCaretPosition((prev) => {
-          return {
-            ...prev,
-            x: prev.x -= 17
-          };
-        })
-        return
 
-      }
+
 
 
 
@@ -286,6 +354,8 @@ export const WritingTest = () => {
     }
   }, [seconds]);
 
+
+
   return (
     <section className="mt-8">
       <h2
@@ -304,8 +374,8 @@ export const WritingTest = () => {
       ) : (
         <>
 
-          <div className=" w-full flex flex-wrap  relative ">
-            <div className="caret" style={{ position: 'absolute', left: '0', transform: `translateX(${caretPosition.x}px)`, transition: 'transform 0.1s ease' }}></div>
+          <div className=" w-full flex flex-wrap   relative " ref={testContent}>
+            <div className="caret" style={{ position: 'absolute', left: caretPosition.x, top: caretPosition.y, transition: 'transform 0.1s ease' }} ref={caret}></div>
             {test.map(({ id, letters, state }) => (
               <div
                 id="word"
