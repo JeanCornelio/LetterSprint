b172caf9fce1c847a5bac5469323086737d91cc6
Jean Cornelio <jean_cornelio17@hotmail.com>
2024-10-23 02:11:59 -0400

    new classes in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,23 +8,23 @@ import { CrownIcon, KeyBoardIcon, UserIcon } from "../icons/Icons";
 
 export const Nav = () => {
   return (
-    <nav className="container px-5">
-      <div className="flex py-7">
-        <a className="mx-auto cursor-pointer ">
+    <nav className="container max-h-24">
+      <div className="flex py-7 justify-between">
+        <Link to='/' className=" cursor-pointer ">
           <h1 className="text-xl lg:text-3xl gap-2 flex font-bold tracking-wide  fade-in-bottom">
             <KeyBoardIcon className=" text-xl lg:text-3xl text-sprint-blue mt-0.5" />{" "}
             LetterSprint
           </h1>
-        </a>
-  
+        </Link>
+
         <div className="flex gap-2 items-center ">
-        <button  className="me-auto  p-1 rounded-full hover:text-sprint-blue transition">
+          <button className="me-auto  p-1 rounded-full hover:text-sprint-blue transition">
             <CrownIcon className="text-xl" />
           </button>
           <Link to='/login' className="  p-1 rounded-full hover:text-sprint-blue transition">
             <UserIcon className="text-xl" />
           </Link>
-       
+
         </div>
       </div>
     </nav>
